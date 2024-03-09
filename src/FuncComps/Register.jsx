85a39398ae251ed086156c8b62a2d564b60e6ade
Philip Shlifer <ben13ph@gmail.com>
2024-03-09 08:32:47 +0200

    validation

--- src/FuncComps/Register.jsx
+++ src/FuncComps/Register.jsx
@@ -11,9 +11,21 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function Register(props) {
-  const [user, setUser] = useState({}); //maybe del
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+    passwordVer: "", //also not needed in localstorage
+    picture: "",
+    firstname: "",
+    lastname: "",
+    email: "",
+    date: "",
+    city: "",
+    street: "",
+    phone: "",
+  });
   const [value, setValue] = useState("");
-  const [errorState, setErrorState] = useState({ state : false});
+  const [errorState, setErrorState] = useState({ state: false });
 
   const VisuallyHiddenInput = styled("input")({
     clip: "rect(0 0 0 0)",
@@ -29,44 +41,95 @@ export default function Register(props) {
 
   //updating the state with the inputs
   const handleChange = (event) => {
+    console.log(event);
     const { id, value } = event.target;
     if (!value) {
-      setErrorState({ ...errorState, [id]: "This field is required" }); // Update error message
+      setErrorState({
+        ...errorState,
+        state: true,
+        [id]: "This field is required",
+      }); // Update error message
       return;
-    } else {
-      setErrorState({ ...errorState, [id]: "" }); // Clear error message
+    } 
+    // Check for English characters only
+    if (id == "username" && (!value.match(/^[a-zA-Z\s\d\-\_!@#$%^&*()+\=\[\]{};':",./<>?]*$/))) {
+      setErrorState({
+        ...errorState,
+        state: true,
+        [id]: "Please enter text in English only.",
+      });
+      return;
     }
+    if (id == "username" && value.length > 60) {
+      setErrorState({
+        ...errorState,
+        state: true,
+        [id]: "Please enter less than 60 characters.",
+      });
+      return;
+    }
+      
+    setErrorState({ ...errorState, state: false, [id]: "" });
     setUser((prevState) => ({ ...prevState, [id]: value }));
+    console.log(errorState);
+    console.log(user);
   };
 
   const numberChange = (newValue) => {
     setValue(newValue);
-    handleChange({ target: { name: "phone", value: newValue } });
+    handleChange({ target: { id: "phone", value: newValue } });
+  };
+
+  const dateChange = (newValue) => {
+    handleChange({target: {id: "date", value: newValue}})
+  }
+
+  const fileChange = (target) => {
+    //console.log('Selected files:', target.files);
+    const { id, files } = target;
+
+    const allowedTypes = ["image/jpeg", "image/jpg"];
+
+    if (allowedTypes.includes(files[0].type)) {
+      handleChange({target: { id: id, value: files[0] }});
+    } else {
+      console.log("wrong file")
+      //handle exception
+    }
   };
 
   //can be called registerUser and the logic put in the if instead
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!user.username) {
-      setErrorState({ ...errorState, [id]: "This field is required" })
-      setErrorState({...errorState, [state]: true })
+    let errors = { state: false };
+    for (let field in user) {
+      if (!user[field]) {
+        errors = { ...errors, state: true, [field]: "This field is required" };
+      }
     }
-    else if (true) {
+    setErrorState(errors);
+    console.log(errorState);
+    // if (!user.username) {
+    //   setErrorState( (prev) =>  prev = {...errorState, state : true, "username" : "This field is required" })
+    // }
+    // if (!user.password) {
+    //   setErrorState((prev) =>  prev = {...errorState, state : true, "password" : "This field is required" })
+    // }
+    if (!errorState.state) {
       registerUser();
-    } else {
-      // Form is invalid, do nothing
     }
   };
 
   //add the user to the user in localstorage
   const registerUser = () => {
     //TO ADD :check if local storage is null otherwise no spread operator
-    if (props.usersProp == null) {
-      localStorage.setItem("users", JSON.stringify([user]));
-      return;
+    let sameUser = props.usersProp.find((temp) => temp.email == user.email)
+    if (sameUser) {
+      return
     }
-    let usersNew = [...props.usersProp, user];
-    localStorage.setItem("users", JSON.stringify(usersNew));
+    //let usersNew = [...props.usersProp, user];
+    //localStorage.setItem("users", JSON.stringify(usersNew));
+    props.sendNewUser(user);
     //needs to be sent to parent!!!!!!!!!!!!!!!!!@@#!#!@#@!#@!
   };
 
@@ -92,7 +155,7 @@ export default function Register(props) {
         <TextField
           required
           //error={errorState}
-          error={!!errorState.username} // Display error if message exists
+          error={errorState.username} // Display error if message exists
           helperText={errorState.username}
           id="username"
           label="Username"
@@ -102,6 +165,8 @@ export default function Register(props) {
         <br />
         <TextField
           required
+          error={errorState.password} // Display error if message exists
+          helperText={errorState.password}
           id="password"
           label="Password"
           variant="standard"
@@ -110,6 +175,8 @@ export default function Register(props) {
         <br />
         <TextField
           required
+          error={errorState.passwordVer} // Display error if message exists
+          helperText={errorState.passwordVer}
           id="passwordVer"
           label="Verify Password"
           variant="standard"
@@ -118,6 +185,7 @@ export default function Register(props) {
         <br />
         <br />
         <Button
+          id="picture"
           component="label"
           role={undefined}
           variant="contained"
@@ -125,11 +193,14 @@ export default function Register(props) {
           startIcon={<CloudUploadIcon />}
         >
           Upload Picture
-          <VisuallyHiddenInput type="file" />
+          <VisuallyHiddenInput id="picture" type="file" accept=".jpg,.jpeg"
+          onChange={(event) => fileChange(event.target)}/>
         </Button>
         <br />
         <TextField
           required
+          error={errorState.firstname} // Display error if message exists
+          helperText={errorState.firstname}
           id="firstname"
           label="First Name"
           variant="standard"
@@ -138,6 +209,8 @@ export default function Register(props) {
         <br />
         <TextField
           required
+          error={errorState.lastname} // Display error if message exists
+          helperText={errorState.lastname}
           id="lastname"
           label="Last Name"
           variant="standard"
@@ -146,6 +219,8 @@ export default function Register(props) {
         <br />
         <TextField
           required
+          error={errorState.email} // Display error if message exists
+          helperText={errorState.email}
           id="email"
           label="Email"
           variant="standard"
@@ -153,11 +228,18 @@ export default function Register(props) {
         />
         <br />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker />
+          <DatePicker
+            id="date"
+            onChange={dateChange}
+            error={errorState.date} // Display error if message exists
+            helperText={errorState.date}
+          />
         </LocalizationProvider>
         <br />
         <TextField
           required
+          error={errorState.city} // Display error if message exists
+          helperText={errorState.city}
           id="city"
           label="City"
           variant="standard"
@@ -166,13 +248,21 @@ export default function Register(props) {
         <br />
         <TextField
           required
+          error={errorState.street} // Display error if message exists
+          helperText={errorState.street}
           id="street"
           label="Street"
           variant="standard"
           onChange={handleChange}
         />
         <br />
-        <MuiTelInput value={value} id="phone" onChange={numberChange} />
+        <MuiTelInput
+          error={errorState.phone} // Display error if message exists
+          helperText={errorState.phone}
+          value={value}
+          id="phone"
+          onChange={numberChange}
+        />
         <br />
         <button type="submit" disabled={errorState.state}>
           Submit
